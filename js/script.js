61cffee3295d207dc94eb3f18aafbbc824e103da
Yash Agrawal <yagrawl2@gmail.com>
2017-05-16 09:32:08 +0530

    in progress background

--- js/script.js
+++ js/script.js
@@ -38,44 +38,9 @@ function getBackground(id){
       console.log(response); 
       var Poster = PosterUrl + response.backdrops[0].file_path;
       console.log(Poster);
-      getImageLightness(Poster,function(brightness){
-        console.log(brightness);
-      });
-      $('body').css("background-image", "url("+Poster+")");
+      $('.background').css("background-image", "url("+Poster+")");
+      $('.background').css("opacity", "0.4");
   });
 }
 
-function getImageLightness(imageSrc,callback) {
-    var img = document.createElement("img");
-    img.src = imageSrc;
-    img.style.display = "none";
-    document.body.appendChild(img);
 
-    var colorSum = 0;
-
-    img.onload = function() {
-        // create canvas
-        var canvas = document.createElement("canvas");
-        canvas.width = this.width;
-        canvas.height = this.height;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this,0,0);
-
-        var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-        var data = imageData.data;
-        var r,g,b,avg;
-
-        for(var x = 0, len = data.length; x < len; x+=4) {
-            r = data[x];
-            g = data[x+1];
-            b = data[x+2];
-
-            avg = Math.floor((r+g+b)/3);
-            colorSum += avg;
-        }
-
-        var brightness = Math.floor(colorSum / (this.width*this.height));
-        callback(brightness);
-    }
-}
