42ad01a3adf0fddb8eddd762bdaceee4cafbe0fc
Yash Agrawal <yagrawl2@gmail.com>
2017-05-16 16:17:16 +0530

    done for now

--- js/script.js
+++ js/script.js
@@ -16,13 +16,14 @@ function randomMovies(){
         random = Math.floor((Math.random() * 19) + 1);
         console.log(response);
         var movieName = response.results[random].title;
-        // var Poster = PosterUrl + response.results[random].poster_path;
+        var PosterX = PosterUrl + response.results[random].poster_path;
         var movieid = response.results[random].id.toString();
         document.getElementById("movie").innerHTML = movieName;
+        $('#poster').css('visibility', 'visible').attr("src", PosterX);
+        $('#poster').css('width', '300px');
         getBackground(movieid);
-        // $('body').css("background-image", "url("+Poster+")");
-        // $('#poster').css('visibility', 'visible').attr("src", Poster);
     });
+
 }
 
 function getBackground(id){
@@ -34,13 +35,81 @@ function getBackground(id){
     "headers": {},
     "data": "{}"
   }
+
+  var cast = {
+    "async": true,
+    "crossDomain": true,
+    "url": BaseUrl + id + '/credits' + APIkey,
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+  }
+
+  var details = {
+    "async": true,
+    "crossDomain": true,
+    "url": BaseUrl + id + APIkey,
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+  }
+
   $.ajax(bg).done(function (response) {
       console.log(response); 
       var Poster = PosterUrl + response.backdrops[0].file_path;
       console.log(Poster);
-      $('.background').css("background-image", "url("+Poster+")");
-      $('.background').css("opacity", "0.4");
+      $('.wrapper').css("background-image", "url("+Poster+")");
+      $('#morebutton').css('visibility', 'visible')
+      $('#carr').css('visibility', 'visible')
+      $('#bd1').attr("src", PosterUrl + response.backdrops[1].file_path);
+      $('#bd2').attr("src", PosterUrl + response.backdrops[2].file_path);
+      $('#bd3').attr("src", PosterUrl + response.backdrops[3].file_path);
+      $('#bd4').attr("src", PosterUrl + response.backdrops[4].file_path);
+      $('#bd5').attr("src", PosterUrl + response.backdrops[5].file_path);
+      //$('.wrapper').css("filter", "opacity('80%')");
+      //$('.wrapper').css("opacity", "0.4");
+  });
+
+  $.ajax(cast).done(function (response) {
+      console.log(response); 
+      var cast1 = PosterUrl + response.cast[0].profile_path;
+      var cast2 = PosterUrl + response.cast[1].profile_path;
+      var cast3 = PosterUrl + response.cast[2].profile_path;
+      var cast4 = PosterUrl + response.cast[3].profile_path;
+      var cast5 = PosterUrl + response.cast[4].profile_path;
+      var cast6 = PosterUrl + response.cast[5].profile_path;
+      console.log(cast1);
+      $('#cast1').css('visibility', 'visible').attr("src", cast1);
+      $('#cast1').css('width', '150px');
+      document.getElementById("cast1Name").innerHTML = response.cast[0].name;
+      $('#cast2').css('visibility', 'visible').attr("src", cast2);
+      $('#cast2').css('width', '150px');
+      document.getElementById("cast2Name").innerHTML = response.cast[1].name;
+      $('#cast3').css('visibility', 'visible').attr("src", cast3);
+      $('#cast3').css('width', '150px');
+      document.getElementById("cast3Name").innerHTML = response.cast[2].name;
+      $('#cast4').css('visibility', 'visible').attr("src", cast4);
+      $('#cast4').css('width', '150px');
+      document.getElementById("cast4Name").innerHTML = response.cast[3].name;
+      $('#cast5').css('visibility', 'visible').attr("src", cast5);
+      $('#cast5').css('width', '150px');
+      document.getElementById("cast5Name").innerHTML = response.cast[4].name;
+      $('#cast6').css('visibility', 'visible').attr("src", cast6);
+      $('#cast6').css('width', '150px');
+      document.getElementById("cast6Name").innerHTML = response.cast[5].name;
+  });
+
+  $.ajax(details).done(function (response) {
+      console.log(response); 
+      document.getElementById("Overview").innerHTML = response.overview;
+      document.getElementById("Genre").innerHTML = 'Genre(s): ' + response.genres[0].name;
+      document.getElementById("Release").innerHTML = 'Release Date: ' + response.release_date;
+      document.getElementById("Runtime").innerHTML = 'Runtime: ' + response.runtime + ' mins';
+      var imdb = document.getElementById('imdb');
+      $('#imdb').css('visibility', 'visible')
+      imdb.setAttribute('href', 'https://www.imdb.com/title/' + response.imdb_id);
+
+
   });
 }
-
 
