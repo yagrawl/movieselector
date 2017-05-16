3106dee13e4be7c598246aafc98d47e442b0c0cd
Yash Agrawal <yagrawl2@gmail.com>
2017-05-16 09:04:04 +0530

    in progress background

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 var APIkey = '?api_key=577e44ee9c54fb29e6c0e28882fc5f53';
 var BaseUrl = 'https://api.themoviedb.org/3/movie/';
 var PosterUrl = 'http://image.tmdb.org/t/p/original';
-var randompage = Math.floor((Math.random() * 30) + 1).toString();
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": BaseUrl + 'popular'  + APIkey  + '&page='+ randompage,
-  "method": "GET",
-  "headers": {},
-  "data": "{}"
-}
 
 function randomMovies(){
+    var randompage = Math.floor((Math.random() * 30) + 1).toString();
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": BaseUrl + 'popular'  + APIkey  + '&page='+ randompage,
+      "method": "GET",
+      "headers": {},
+      "data": "{}"
+    }
     $.ajax(settings).done(function (response) {
         random = Math.floor((Math.random() * 19) + 1);
         console.log(response);
